58315b39d8ac1b41c472f614fd6ef2c1bb4a73e1
Frank Gerold <FrankETSGerold@gmail.com>
2019-10-31 15:34:01 -0400

    Finished lab!

--- lib/person.rb
+++ lib/person.rb
@@ -71,6 +71,21 @@ class Person
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
+    def start_conversation ( friend, topic )
+      # binding.pry
+      if topic == "politics"
+        self.happiness -= 2
+        friend.happiness -= 2
+        "blah blah partisan blah lobbyist"
 
+      elsif topic == "weather"
+        self.happiness += 1
+        friend.happiness += 1
+        "blah blah sun blah rain"
+
+      else
+        "blah blah blah blah blah"
+      end
+    end
 
 end
